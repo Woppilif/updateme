d971d1292005b375c72f67ac409b58d4d0c81d3a
woppilif <blink.kerr@gmail.com>
2019-10-23 17:39:33 +0300

    okay2

--- client.py
+++ client.py
@@ -25,7 +25,7 @@ logging.basicConfig(filename='example.log',level=logging.DEBUG,format=FORMAT)
 APP_KEY = config['DEFAULT']['app']
 
 async def hello():
-    async with websockets.connect("ws://127.0.0.1:8000/ws/chat/{0}/".format(config['DEFAULT']['chat_id'])) as websocket:
+    async with websockets.connect("ws://www.ewtm.ru/ws/chat/{0}/".format(config['DEFAULT']['chat_id'])) as websocket:
         print("Listening....")
         greeting = await websocket.recv()
         greeting = greeting.replace("'", "\"")
@@ -33,7 +33,7 @@ async def hello():
         message = greeting['message'].replace("\"",'')
         if message == 'update':
             logging.info('Update...')
-            os.system('git clone https://github.com/Woppilif/updateme.git .')
+            os.system('sudo sh update.sh')
         elif message == APP_KEY:
             
             GPIO.output(2, True)
