c55ea83199f6ee090aa7267feff0bbfe86a77488
woppilif <blink.kerr@gmail.com>
2019-10-23 18:22:28 +0300

    update

---
+++ update.sh
@@ -0,0 +1,8 @@
+sudo rm -r updateme
+git clone https://github.com/Woppilif/updateme.git
+sudo cp updateme/client.py ./
+sudo chmod 777 client.py
+sudo cp updateme/update.sh ./
+sudo chmod 777 update.sh
+sudo reboot
+#okay
