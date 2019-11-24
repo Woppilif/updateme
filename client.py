aff24ff72eab58ad1b309987c9167f3e0656ef0b
Your Name <you@example.com>
2019-11-25 01:20:04 +0300

    bug fix

--- client.py
+++ client.py
@@ -1,9 +1,7 @@
-import asyncio
 import websockets
-from websockets.exceptions import InvalidStatusCode, ConnectionClosedError, InvalidMessage
+import asyncio
 import os
 import configparser
-import time
 import json
 import logging
 try:
@@ -15,24 +13,103 @@ try:
     GPIO.setup(3, GPIO.OUT)
     GPIO.output(3, False)
     GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(5, GPIO.IN ,pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(5, GPIO.IN ,pull_up_down=GPIO.PUD_DOWN)
     GP_APP = True
 except:
     GP_APP = False
 
 FORMAT = '%(asctime)-15s  %(message)s'
-class Flat():
+class WebSocketClient():
+
     def __init__(self):
         config = configparser.ConfigParser()
         config.read("conf.config")
         self.cleint_id = config['DEFAULT']['chat_id']
         self.app_key = config['DEFAULT']['app']
-        self.message = None
-        self.ws_object = websockets.connect("ws://ewtm.ru/ws/chat/{0}/".format(self.cleint_id))
         logging.basicConfig(filename="flat-{0}.log".format(self.cleint_id),level=logging.WARNING,format=FORMAT)
-    
-    def update(self):
+
+    async def connect(self):
+        '''
+            Connecting to webSocket server
+
+            websockets.client.connect returns a WebSocketClientProtocol, which is used to send and receive messages
+        '''
+        self.connection = await websockets.client.connect("ws://ewtm.ru/ws/chat/{0}/".format(self.cleint_id))
+        if self.connection.open:
+            print('Connection stablished. Client correcly connected')
+            # Send greeting
+            await self.sendMessage('Hey server, this is webSocket client')
+            return self.connection
+
+
+    async def sendMessage(self, message):
+        '''
+            Sending message to webSocket server
+        '''
+        await self.connection.send(message)
+
+    async def receiveMessage(self, connection):
+        '''
+            Receiving all server messages and handling them
+        '''
+        while True:
+            try:
+                message = json.loads(await connection.recv())
+                if message['appid'].replace("\"",'') == self.app_key:
+                    if message['message'].replace("\"",'') == 'open':
+                        await self.openDoor()
+                    elif message['message'].replace("\"",'') == 'update':
+                        await self.update()
+                    elif message['message'].replace("\"",'') == 'sendlog':
+                        #await self.sendLog()
+                        await self.sendMessage('LOG HAS BEEN SENT!')
+                    else:
+                        pass
+                else:
+                    print("Wrong APP KEY")
+                    logging.error('Wrong APP KEY')
+                print('Received message from server: ' + str(message))
+            except websockets.exceptions.ConnectionClosed:
+                print('Connection with server closed')
+                break
+
+    async def heartbeat(self, connection):
+        '''
+        Sending heartbeat to server every 5 seconds
+        Ping - pong messages to verify connection is alive
+        '''
+        while True:
+            '''
+            if GP_APP:
+                if int(GPIO.input(4)) == 1:
+                    print("box opened")
+                    await self.sendMessage("box opened")
+                    await asyncio.sleep(5)
+                if int(GPIO.input(5)) == 1:
+                    print("door opened")
+                    await self.sendMessage("door opened")
+                    await asyncio.sleep(5)
+            '''
+            
+            try:
+                await connection.ping()
+                print("it was ping")
+                await asyncio.sleep(5)
+            except websockets.exceptions.ConnectionClosed:
+                print('Connection with server closed')
+                break
+
+    async def openDoor(self):
+        if GP_APP:
+            GPIO.output(2, True)
+            time.sleep(5)
+            GPIO.output(2, False)
+        logging.warning('Door opened...')
+        await self.sendMessage('openDoor')
+
+    async def update(self):
         logging.warning('Update...')
+        await self.sendMessage('updating software....')
         if GP_APP:
             os.system('sudo rm -r updateme')
             os.system('git clone https://github.com/Woppilif/updateme.git')
@@ -41,100 +118,22 @@ class Flat():
             os.system('sudo cp updateme/update.sh ./')
             os.system('sudo chmod 777 update.sh')
             os.system('sudo reboot')
-        
-
-    def openDoor(self):
-        if GP_APP:
-            GPIO.output(2, True)
-            time.sleep(5)
-            GPIO.output(2, False)
-        
-        logging.warning('Door opened...')
-        print("opened!")
-
-    async def sendLog(self):
-        fil = []
-        async with open("flat-{0}.log".format(self.cleint_id), 'r') as f:
-            await self.sendMessage(str(f))
-        
-        
-
-    async def sendMessage(self,message):
-        async with self.ws_object as websocket:
-            print("Message has been sent!")
-            await websocket.send(message)
-
-    async def readMessage(self):
-        self.message = json.loads(self.message)
-        if self.message['appid'].replace("\"",'') == self.app_key:
-            if self.message['message'].replace("\"",'') == 'open':
-                self.openDoor()
-                await self.sendMessage('openDoor')
-            elif self.message['message'].replace("\"",'') == 'update':
-                self.update()
-                await self.sendMessage('updating software....')
-            elif self.message['message'].replace("\"",'') == 'sendlog':
-                #await self.sendLog()
-                await self.sendMessage('LOG HAS BEEN SENT!')
-            else:
-                pass
-        else:
-            logging.error('Wrong APP KEY')
-        self.message = None
-
-    async def getMessage(self):
-        async with self.ws_object as websocket:
-            await asyncio.sleep(0)
-            print("Listening...")
-            while True:
-                self.message = await websocket.recv()
-                await self.readMessage()
-
-    async def answer(self):
-        print("File destructor is here")
-        while True:
-            if GP_APP:
-                if int(GPIO.input(4)) == 0:
-                    print("box opened")
-                    await asyncio.sleep(10)
-                    await self.sendMessage("box opened")
-                if int(GPIO.input(5)) == 1:
-                    print("door opened")
-                    await asyncio.sleep(5)
-                    await self.sendMessage("door opened")
-            await asyncio.sleep(0)
-           
-
-    async def Run(self):
-        while True:
-            try:
-                await self.getMessage()
-            except ConnectionClosedError:
-                print("Disconnected! Trying to reconnect!")
-                logging.error('Disconnected! Trying to reconnect!')
-                await asyncio.sleep(5)
-            except InvalidStatusCode:
-                print("InvalidStatusCode! Trying to connect to server.....!")
-                logging.error('InvalidStatusCode! Trying to connect to server.....!')
-                await asyncio.sleep(5)
-            except ConnectionRefusedError:
-                print("ConnectionRefusedError! Trying to connect to server.....!")
-                logging.error('ConnectionRefusedError! Trying to connect to server.....!')
-                await asyncio.sleep(5)
-            except InvalidMessage:
-                print("InvalidMessage! Trying to connect to server.....!")
-                logging.error('InvalidMessage! Trying to connect to server.....!')
-                await asyncio.sleep(5)
 
 
-while True:
-    
-    flat = Flat()
-    loop = asyncio.get_event_loop()
-    tasks = []
-    tasks.append(loop.create_task(flat.Run()))
-    tasks.append(loop.create_task(flat.answer()))
-    x = asyncio.gather(*tasks)
-    loop.run_until_complete(x)
-    
-  
+if __name__ == '__main__':
+    # Creating client object
+    while True:
+        try:
+            client = WebSocketClient()
+            loop = asyncio.get_event_loop()
+            # Start connection and get client connection protocol
+            connection = loop.run_until_complete(client.connect())
+            # Start listener and heartbeat 
+            tasks = [
+                asyncio.ensure_future(client.heartbeat(connection)),
+                asyncio.ensure_future(client.receiveMessage(connection)),
+            ]
+
+            loop.run_until_complete(asyncio.wait(tasks))
+        except:
+            pass
